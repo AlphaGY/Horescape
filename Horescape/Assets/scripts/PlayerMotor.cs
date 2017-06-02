dbb7c64828c0d645290ceab0caf99e0c628fe7d7
AlphaGY <stan-_-1@163.com>
2017-06-01 20:45:31 -0700

    [YG] prototype: 1.move forward and speed up automatically 2.move left/right according to the input 3.fall when there is a hole 4.blocked when there is a barrier

---
+++ Horescape/Assets/scripts/PlayerMotor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMotor : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 movement;
+    private float forwardSpeed = 5.0f;
+    // accelerated speed
+    private float accelerated = 5.0f;
+    // countdown for accelerating
+    private float countdown;
+    private float fallingSpeed = 0.0f;
+    private float gravity = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        countdown = accelerated;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Vector3.zero;
+        // speed up every [accelerated] seconds
+        if (countdown > 0)
+        {
+            countdown -= (accelerated * Time.deltaTime);
+        }
+        else
+        {
+            forwardSpeed += 0.2f;
+            countdown = accelerated;
+        }
+        movement.z = forwardSpeed;
+        // left & right movement from input
+        movement.x = Input.GetAxisRaw("Horizontal") * -5.0f;
+        // gravity
+        if (controller.isGrounded)
+        {
+            fallingSpeed = 0.0f;
+        }
+        else
+        {
+            fallingSpeed -= gravity;
+        }
+        movement.y = fallingSpeed;
+
+        controller.Move(movement * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Hand"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        forwardSpeed -= 1.0f;
+    }
+}
