2529fafcbe7c01e49c8e0e87a42cf70ace9bc60c
os x <osx@ostekiMacBook-Air.local>
2017-07-09 02:37:15 -0700

    hand and audio

---
+++ Horescape/Assets/audio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour {
+
+	//music src
+	public AudioSource music;
+	//volume
+	public float musicVolume;	
+
+	void Start() {
+		//initial volume
+		musicVolume = 0.5F;
+
+	}
+
+	public void AudioOn() {
+
+		music.Play ();
+
+	}
+	public void AudioOff(){
+		music.Stop();
+	}
+}
